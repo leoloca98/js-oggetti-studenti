f2eb1d706440de05c7e01923b567db5ba857ed02
leoloca98 <leonardolocatelli98@gmail.com>
2021-07-19 15:58:41 +0200

    Es 2 Complete

--- js/script-2.js
+++ js/script-2.js
@@ -7,3 +7,46 @@ Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo
 oggetto studente inserendo nell’ordine: nome, cognome e età.
 
 */
+//FORM
+studentiDisplay = document.getElementById("student");
+var x;
+var y;
+
+//*Creazione di array di oggenti di studenti
+var studenti = [
+    {
+        nome: "Leonardo",
+        cognome: "Locatelli",
+        eta: "23",
+    },
+    {
+        nome: "Pasquale",
+        cognome: "Raso",
+        eta: "38",
+    },
+    {
+        nome: "Giorgio",
+        cognome: "Andrei",
+        eta: "19",
+    }
+]
+
+//*Stampare ogni proprietà dei singoli studenti
+for (i = 0; i < studenti.length; i++) {
+    console.log(studenti[i]);
+}
+
+var userNameChoice = prompt("Inserire nome:");
+var userLastnameChoice = prompt("Inserire cognome:");
+var userAgeChoice = prompt("Inserire età:");
+
+var newStudent = {
+    nome: userNameChoice,
+    cognome: userLastnameChoice,
+    eta: userAgeChoice,
+}
+
+studenti.push(newStudent);
+console.table(studenti);
+
+
